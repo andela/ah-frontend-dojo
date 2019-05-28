0f8d561fd222f55f1e96f5dbd6cef5c7e81d548b
Arthur Kalule <arthur.kalule@andela.com>
2019-05-28 14:28:03 +0300

    Chore(setup):setting up sass - add sass processing on client - add sass/css linter
    [Maintains #165273522]

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' ,// compiles Sass to CSS, using Node Sass by default
+        ],
+      },
     ],
   },
   plugins: [
